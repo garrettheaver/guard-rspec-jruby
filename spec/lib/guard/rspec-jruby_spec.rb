6d9741ea3bfe1928b8666249802870737367b9dd
Garrett Heaver <garrett@iterationfour.com>
2012-07-04 19:35:36 +0100

    inlining the text for the jruby runner specs

--- spec/lib/guard/rspec-jruby_spec.rb
+++ spec/lib/guard/rspec-jruby_spec.rb
@@ -4,36 +4,22 @@ require_relative '../../../lib/guard/rspec-jruby'
 module Guard
   describe RSpecJRubyRunner do
 
-    PASSING_SPEC = <<-EOF
-      describe 'a passing spec' do
-        it 'is true that true is true' do
-          true.should be_true
-        end
-      end
-    EOF
-
-    FAILING_SPEC = <<-EOF
-      describe 'a failing spec' do
-        it 'is false that false is true' do
-          false.should be_true
-        end
-      end
-    EOF
-
-    ERROR_SPEC = <<-EOF
-      describe 'a spec with a syntax error'
-        it 'is desirable that the guard not crash when syntax errors occur' do
-          true.should be_true
-        end
-      end
-    EOF
-
     let(:runner) { ::Guard::RSpecJRubyRunner.new }
 
     it 'returns true from run_via_shell when the specs pass' do
       Dir.mktmpdir do |dir|
         path = File.join(dir, 'passing_spec.rb')
-        File.open(path, 'w') { |f| f.write(PASSING_SPEC) }
+
+        File.open(path, 'w') do |f|
+          f.write <<-EOF
+            describe 'a passing spec' do
+              it 'is true that true is true' do
+                true.should be_true
+              end
+            end
+          EOF
+        end
+
         runner.run_via_shell([path], {}).should be_true
       end
     end
@@ -41,7 +27,17 @@ module Guard
     it 'returns false from run_via_shell whent he specs fail' do
       Dir.mktmpdir do |dir|
         path = File.join(dir, 'failing_spec.rb')
-        File.open(path, 'w') { |f| f.write(FAILING_SPEC) }
+
+        File.open(path, 'w') do |f|
+          f.write <<-EOF
+            describe 'a failing spec' do
+              it 'is false that false is true' do
+                false.should be_true
+              end
+            end
+          EOF
+        end
+
         runner.run_via_shell([path], {}).should be_false
       end
     end
@@ -49,7 +45,17 @@ module Guard
     it 'gracefully handles error ocurring with the container' do
       Dir.mktmpdir do |dir|
         path = File.join(dir, 'failing_spec.rb')
-        File.open(path, 'w') { |f| f.write(ERROR_SPEC) }
+
+        File.open(path, 'w') do |f|
+          f.write <<-EOF
+            describe 'a spec with a syntax error'
+              it 'is desirable that the guard not crash when syntax errors occur' do
+                true.should be_true
+              end
+            end
+          EOF
+        end
+
         UI.should_receive(:error).with(/SyntaxError/)
         runner.run_via_shell([path], {}).should be_false
       end
